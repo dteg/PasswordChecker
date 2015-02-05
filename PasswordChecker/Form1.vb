705db0b30d2b875a41d20736a152f4f652baab38
dteg <d-teg@Hotmail.com>
2015-02-05 18:44:53 -0500

    added a limit for entering passwords

--- PasswordChecker/Form1.vb
+++ PasswordChecker/Form1.vb
@@ -1,9 +1,12 @@
 ﻿Imports System.Text.RegularExpressions
 
 
+
 Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Static count As Integer
+        count = count + 1
         Dim Str As String
         Dim clean As String
         Dim rev As String
@@ -11,11 +14,21 @@ Public Class Form1
         Str = UCase(Str)
         clean = Regex.Replace(Str, "[^A-Za-z\-/]", "")
         rev = StrReverse(clean)
-        If (Len(clean) > 5) Then
-            If (clean.Equals(rev)) Then
-                Dim f2 As New Form2
-                f2.Show()
+        If (count < 3) Then
+            If (Len(clean) > 5) Then
+                If (clean.Equals(rev)) Then
+                    Dim f2 As New Form2
+                    f2.Show()
+                End If
             End If
+        Else
+            Dim f3 As New Form3
+            f3.Show()
         End If
     End Sub
+
+    Private Sub count(p1 As Object)
+        Throw New NotImplementedException
+    End Sub
+
 End Class
