f1647ded125de6ee28ba6391a5e523efe6743e17
dteg <d-teg@Hotmail.com>
2015-02-05 18:22:51 -0500

    first setup completed

---
+++ PasswordChecker/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Text.RegularExpressions
+
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Str As String
+        Dim clean As String
+        Dim rev As String
+        Str = TextBox1.Text
+        Str = UCase(Str)
+        clean = Regex.Replace(Str, "[^A-Za-z\-/]", "")
+        rev = StrReverse(clean)
+        If (Len(clean) > 5) Then
+            If (clean.Equals(rev)) Then
+                Dim f2 As New Form2
+                f2.Show()
+            End If
+        End If
+    End Sub
+End Class
